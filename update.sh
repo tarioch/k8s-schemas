7813c5d62c29214ed9ec91b6f32da6e2cde344d5
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-24 23:04:36 +0200

    add schema for jaeger

--- update.sh
+++ update.sh
@@ -17,6 +17,8 @@ cd schemas
 
 python3 ../openapi2jsonschema.py ../crds.yaml
 
+python3 ../openapi2jsonschema.py https://doc.crds.dev/raw/github.com/jaegertracing/jaeger-operator
+
 cd ..
 
 #rm crds.yaml
